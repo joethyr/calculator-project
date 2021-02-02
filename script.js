d21b3677fce9c53629dc900b4c330d0de0f5f77a
joethyr <joethayr@gmail.com>
2021-02-01 17:48:10 -0700

    added function that display the button number clicked

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const buttonNumber = document.querySelectorAll(".btn__number");
+
+let screenValue = "";
+
 function add(a, b) {
   return a + b;
 }
@@ -13,3 +17,14 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function appendNumber(number) {
+  screenValue += number;
+  document.querySelector(".screen__value").innerHTML = screenValue;
+}
+
+buttonNumber.forEach(function (button) {
+  button.addEventListener("click", function () {
+    appendNumber(button.textContent);
+  });
+});
